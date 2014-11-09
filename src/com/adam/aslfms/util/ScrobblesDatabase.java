3c81af0a50001efd2cd9e230246fbf1db39683a8
Malachi Soord <inverse.chi@gmail.com>
2014-11-09 12:48:45 +0100

    Fixed indentation

--- src/com/adam/aslfms/util/ScrobblesDatabase.java
+++ src/com/adam/aslfms/util/ScrobblesDatabase.java
@@ -305,7 +305,7 @@ public class ScrobblesDatabase {
 	public int queryNumberOfTracks() {
 		if(mDb == null || !mDb.isOpen()) {
 			open();
-    	}
+		}
 		Cursor c = mDb.rawQuery("select count(_id) from scrobbles", null);
 		int count = c.getCount();
 		if (count != 0) {
