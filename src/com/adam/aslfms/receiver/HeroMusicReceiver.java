f5fcce0546b616e0d44adcbd7005997a021d570b
HumbleBeeBumbleBee <HumbleBeeBumbleBeeDebugs@gmail.com>
2015-05-04 13:31:41 -0400

    Update HeroMusicReceiver.java

--- src/com/adam/aslfms/receiver/HeroMusicReceiver.java
+++ src/com/adam/aslfms/receiver/HeroMusicReceiver.java
@@ -37,9 +37,4 @@ public class HeroMusicReceiver extends BuiltInMusicAppReceiver {
 	public HeroMusicReceiver() {
 		super(ACTION_HTC_STOP, "com.htc.music", "Hero Music Player");
 	}
-
-	@Override
-	protected String getMetaChangedAction() {
-		return ACTION_HTC_METACHANGED;
-	}
 }
