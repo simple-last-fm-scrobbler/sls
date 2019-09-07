3ceb45a43092cd338d9809deb2e1aa1fc34328ea
Austin Hogan <names.a93@use.startmail.com>
2019-09-07 15:54:17 -0400

    Name Change & Version Update
    as in title

--- app/src/main/java/com/adam/aslfms/util/NotificationCreator.java
+++ app/src/main/java/com/adam/aslfms/util/NotificationCreator.java
@@ -1,3 +1,23 @@
+/**
+ * This file is part of Simple Scrobbler.
+ * <p>
+ * https://github.com/simple-last-fm-scrobbler/sls
+ * <p>
+ * Copyright 2011 Simple Scrobbler Team
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.adam.aslfms.util;
 
 import android.app.Notification;
@@ -16,7 +36,10 @@ import com.adam.aslfms.R;
 import com.adam.aslfms.SettingsActivity;
 import com.adam.aslfms.service.NetworkerManager;
 import com.adam.aslfms.service.ScrobblingService;
-
+/**
+ * @author a93h
+ * @since 1.5.8
+ */
 public class NotificationCreator {
 
     private static final String TAG = "NotificationCreator";
